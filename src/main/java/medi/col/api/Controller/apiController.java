edb4e32ae0db95f3d978a6edb9c52c3f9e98c7f2
PotenzaFeli <potenzafelipe@gmail.com>
2025-01-11 19:37:45 -0300

    Adicionando o Swagger  e Iniciando preparos para interfaces

--- src/main/java/medi/col/api/Controller/apiController.java
+++ src/main/java/medi/col/api/Controller/apiController.java
@@ -1,15 +1,18 @@
 package medi.col.api.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+
+@Controller
 @RequestMapping("/api")
 public class apiController {
     
-    @GetMapping
+    @GetMapping("/index")
     public String Inicial(){
-        return "Teste";
+        return "index";
     }
+    
 }
