1f490be82e9449aa29a40e04fce7e1977c513230
PotenzaFeli <potenzafelipe@gmail.com>
2025-01-14 21:36:28 -0300

    Adicionando CrossOrigin e correções Em algumas telas

--- src/main/java/medi/col/api/Controller/PacienteController.java
+++ src/main/java/medi/col/api/Controller/PacienteController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ import medi.col.api.Paciente.dadosCadastroPaciente;
 
 @RestController
 @RequestMapping("/paciente")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PacienteController {
 
     @Autowired
