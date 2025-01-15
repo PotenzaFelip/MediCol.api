096b6c99c7a33aa560acf8500c9f7907f84db473
PotenzaFeli <potenzafelipe@gmail.com>
2025-01-14 22:17:05 -0300

    Removendo Interfaces

--- src/main/java/medi/col/api/Controller/apiController.java
+++ src/main/java/medi/col/api/Controller/apiController.java
@@ -1,38 +1,8 @@
 package medi.col.api.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-
 
 @Controller
 public class apiController {
-    
-    @GetMapping("/index")
-    public String Inicial(){
-        return "index";
-    }
-    @GetMapping("/medicos")
-    public String medicos(){
-        return "medicos";
-    }
-    @GetMapping("/pacientes")
-    public String pacientes(){
-        return "pacientes";
-    }
-    @GetMapping("/cadmedicos")
-    public String cadmedicos() {
-        return "cadmedicos";
-    }
-    @GetMapping("/listamedicos")
-    public String listmedicos() {
-        return "listamedicos";
-    }
-    @GetMapping("/updatemedicos")
-    public String updatemedicos() {
-        return "updatemedicos";
-    }
-    
 
 }
